89944c01a5dcfed8919625c1adc4a169bf3f072b
lamantovani@gmail.com <lamantovani@gmail.com>
2024-05-11 10:52:12 -0300

    Ajuste de configurações do dispatcher

--- service-fraud-detector/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
+++ service-fraud-detector/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
@@ -2,7 +2,6 @@ package br.com.alura.ecommerce;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.common.serialization.StringDeserializer;
 
 import java.util.Map;
 
@@ -12,13 +11,13 @@ public class FraudDetectorService {
         var fraudDetectorService = new FraudDetectorService();
         try (var service = new KafkaService<>(FraudDetectorService.class.getSimpleName(),
                 "ECOMMERCE_NEW_ORDER", fraudDetectorService::parse,
-                String.class,
-                Map.of(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName()))) {
+                Order.class,
+                Map.of(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, GsonDeserializer.class.getName()))) {
             service.run();
         }
     }
 
-    private void parse(ConsumerRecord<String, String> record) {
+    private void parse(ConsumerRecord<String, Order> record) {
         System.out.println("-------------------------------------------------");
         System.out.println("Processing new order, checking for fraud");
         System.out.println("KEY: " + record.key());
--- service-new-order/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ service-new-order/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -13,19 +13,18 @@ public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         try (var orderDispatcher = new KafkaDispatcher<Order>()) {
-            try (var emailDispatcher = new KafkaDispatcher<Email>()) {
+            try (var emailDispatcher = new KafkaDispatcher<String>()) {
 
                 for (int i = 0; i < 10; i++) {
 
                     var userId = UUID.randomUUID().toString();
-                    var emailId = UUID.randomUUID().toString();
                     var orderId = UUID.randomUUID().toString();
                     var amount = new BigDecimal(Math.random() * 5000 + 1);
                     var order = new Order(userId, orderId, amount);
-
                     orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
 
-                    var email = new Email("teste", "Thank you for your order! We are processing your order!");
+                    var emailId = UUID.randomUUID().toString();
+                    var email = "Thank you for your order! We are processing your order!";
                     emailDispatcher.send("ECOMMERCE_SEND_EMAIL", emailId, email);
                 }
             }
